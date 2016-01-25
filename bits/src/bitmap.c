844c73b45dd72e4e419f5c21e1c4031561698f00
joemelt101 <joewebsites@outlook.com>
2016-01-24 18:41:16 -0600

    bitmap_create passed tests

--- bits/src/bitmap.c
+++ bits/src/bitmap.c
@@ -1,4 +1,6 @@
 #include "../include/bitmap.h"
+#include <limits.h>
+#include <math.h>
 
 
 struct bitmap 
@@ -9,14 +11,43 @@ struct bitmap
 
 bitmap_t *bitmap_create(size_t n_bits) 
 {
+    //TODO: why is SIZE_MAX too large?
+    if (n_bits == 0 || n_bits == SIZE_MAX)
+    {
+        return NULL;
+    }
     
-	return NULL;
+    //allocate a bitmap_t struct
+    bitmap_t *map = (bitmap_t *) malloc(sizeof(bitmap_t));
+    
+    if (map == NULL)
+    {
+        //was unable to allocate for some reason....
+        return NULL;
+    }
+
+    //allocate the given number of bits
+    map->bit_count = n_bits;
+    map->byte_count = n_bits / 8;
+    
+    //allocate data. Make sure it is cleared out (calloc)!
+    map->data = (uint8_t *) calloc(map->byte_count, 1);
+    
+	return map;
 }
 
 bool bitmap_set(bitmap_t *const bitmap, const size_t bit) 
 {
+    if (! bitmap)
+    {
+        //must be instantiated!
+        return false;
+    }
     
-	return false;
+    //ceil(bitmap->data)
+    
+    //made it to the end so success!
+	return true;
 }
 
 bool bitmap_reset(bitmap_t *const bitmap, const size_t bit) 
