73107bc78623ea63a91bd52a2e9c047a5ed3cc32
joemelt101 <joewebsites@outlook.com>
2016-02-01 20:16:31 -0600

    Finished the allocation module.

--- allocation/src/allocation.c
+++ allocation/src/allocation.c
@@ -1,21 +1,76 @@
 #include "../include/allocation.h"
+#include "stdlib.h"
+#include "stdio.h"
 
 void* allocate_array(size_t member_size, size_t nmember,bool clear)
 {
-	return NULL;
+    if (member_size == 0 || nmember == 0)
+    {
+        return NULL;
+    }
+    
+    void *arr;
+    
+    if (clear)
+    {
+        arr = calloc(nmember, member_size);
+    }
+    else
+    {
+        arr = malloc(nmember * member_size);
+    }
+    
+	return arr;
 }
 
 void* reallocate_array(void* ptr, size_t size)
 {
-	return NULL;
+    if (size == 0)
+    {
+        return NULL;
+    }
+    
+	return realloc(ptr, size);    
 }
 
 void deallocate_array(void** ptr)
 {
-
+    if (! ptr)
+    {
+        return;
+    }
+    
+    free(*ptr);
+    *ptr = NULL;
 }
 
 char* read_line_to_buffer(char* filename)
 {
-	return NULL;
+    //get size of file
+    
+    FILE* file = fopen(filename, "r");
+    
+    //check to see if got the file OK
+    if (file == NULL)
+    {
+        //failed to open file...
+        return NULL;
+    }
+    
+    char* line = NULL;
+    size_t n = 0;
+	
+    int res = getline(&line, &n, file);
+    
+    //close the file
+    fclose(file);
+    
+    //if error occurred
+    if (res == -1)
+    {
+        return NULL;
+    }
+    
+    //else return line
+    return line;
 }
